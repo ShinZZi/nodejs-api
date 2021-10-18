925a3d3967e499f548579d21d9d9fd6ec8105b87
shinzzi <longnp1610@gmail.com>
2021-10-19 00:16:50 +0700

    fix sth

--- main.js
+++ main.js
@@ -15,7 +15,14 @@ mongoose
   .catch((err) => {
     console.log(Error, "DB Connect Failure");
   });
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
